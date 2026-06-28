5d6d745e11271d946edb2d50e0eda53bdb904050
PROMPT Ai <128673231+userchriso@users.noreply.github.com>
2026-06-28 19:22:24 +0200

    Add files via upload

---
+++ PMBSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PMBSAApp.swift
+//  PMBSA
+//
+//  Created by Chris Olivier on 27/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PMBSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
